b732a15da2716380531698e5872848a4e5f6d1c3
totem <totem@totem.totem.com>
2021-05-28 11:01:25 -0300

    adsf

--- imports/ui/Dashboard/Respuestas/RtaComboUpdate.jsx
+++ imports/ui/Dashboard/Respuestas/RtaComboUpdate.jsx
@@ -58,7 +58,7 @@ export default class RtaComboUpdate extends Component {
     //  console.log("estado: ", this.state.valor);
     //  console.log("estado anterior: ", prevProps.rta.rtatexto);
     //agregue la primer condición de la expresión porque a veces rta es undefined
-    if (this.props.rta && this.props.rta.rtatexto !== prevProps.rta.rtatexto)
+    if (this.props.rta.rtatexto !== prevProps.rta.rtatexto)
       this.setState({ valor: this.props.rta.rtatexto });
   }
 
